6c9a2c1b17b357a2cd4575584077466c21285b85
akbansal765 <ak.bansal.765@gmail.com>
2025-08-05 20:35:19 +0530

    added lazy components

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx';
+// import App from './App.jsx';
+// import MainContainer from './Components/MainContainer.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import appStore from './utils/store.js';
 import { Provider } from 'react-redux';
-import MainContainer from './Components/MainContainer.jsx';
 import NotFound from './Components/NotFound.jsx';
+import { lazy, Suspense } from 'react';
+import spinnerGif from "./assets/spinner.gif"
+
+const App = lazy(() => import("./App.jsx"))
+const MainContainer = lazy(() => import("./Components/MainContainer.jsx"))
+
+// lazy fallback spinner
+const spinner = (
+  <div style={{ textAlign: 'center', padding: '2rem'}}>
+    <img src={spinnerGif} alt="Loading..." width="80" />
+  </div>
+);
+
 
 const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
+      element: <Suspense fallback={spinner}><App/></Suspense>,
       errorElement: <NotFound />
     },
     {
       path: "/mainContainer",
-      element: <MainContainer />
+      element: <Suspense fallback={spinner}><MainContainer /></Suspense>
     }
 ]);
 
