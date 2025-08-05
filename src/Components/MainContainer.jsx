1d5c884e94a96d0dff89fca364778142f146effd
akbansal765 <ak.bansal.765@gmail.com>
2025-08-05 20:55:15 +0530

    lazy components bug

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,21 +1,10 @@
 import Header from "./Header";
-// import TaskForm from "./TaskForm";
-// import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 import { getTasksDB } from "../utils/taskSlice";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { lazy, Suspense } from 'react';
-import spinnerGif from "../assets/spinner.gif"
 
-const TaskForm = lazy(() => import("./TaskForm"));
-const TaskList = lazy(() => import("./TaskList"));
-
-// lazy fallback spinner
-const spinner = (
-  <div style={{ textAlign: 'center', padding: '2rem'}}>
-    <img src={spinnerGif} alt="Loading..." width="80" />
-  </div>
-);
 
 function MainContainer(){
     const dispatch = useDispatch();
@@ -29,8 +18,8 @@ function MainContainer(){
         <div className="mainContainer_component">
            <Header />
            <div className="taskForm_taskList_box">
-             <Suspense fallback={spinner}><TaskForm /></Suspense>
-             <Suspense fallback={spinner}><TaskList /></Suspense>
+             <TaskForm />
+             <TaskList />
            </div>
         </div>
     )
